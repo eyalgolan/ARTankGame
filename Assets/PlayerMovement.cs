642c10ff58f203cf71ac621a70c1f96454c08247
Eyal Golan <eyalgolan96@gmail.com>
2019-10-31 22:03:03 +0200

    tanks now coliding with boundries and each other

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public float playerNumber;
