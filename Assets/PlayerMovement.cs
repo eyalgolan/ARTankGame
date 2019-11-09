a2e7d31456581305d620defb481d19cb923b1533
Eyal Golan <eyalgolan96@gmail.com>
2019-11-09 20:06:10 +0200

    fixed sound, colliding still not working

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -25,4 +25,22 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
     }
+
+    void OnCollisionEnter(Collision c)
+    {
+        // force is how forcefully we will push the player away from the enemy.
+        float force = 3;
+ 
+        // If the object we hit is the enemy
+        if (c.gameObject.tag == "enemy")
+        {
+            // Calculate Angle Between the collision point and the player
+             Vector3 dir = c.contacts[0].point - transform.position;
+             // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+             // And finally we add force in the direction of dir and multiply it by force. 
+             // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir*force);
+        }
+    }
 }
